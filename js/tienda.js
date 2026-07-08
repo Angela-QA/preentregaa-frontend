de09a2988e4c04dac03150bf113f3044d60ce8e6
Angela <aimendz@hotmail.com>
2026-07-08 07:35:49 -0300

    Termine fcion carrito y actualice el total en pantalla

--- js/tienda.js
+++ js/tienda.js
@@ -1,6 +1,8 @@
 let carrito= [];
 
 const contenedorTarjetas= document.querySelector('.productos-seccion');
+const listacarrito= document.querySelector('.carrito__lista');
+const totalcarrito= document.querySelector('#carrito_total-valor');
 
 catalogoAventura.forEach((producto)=>{
     contenedorTarjetas.innerHTML +=`
@@ -17,7 +19,25 @@ contenedorTarjetas.addEventListener("click",(e)=>{
         const idSeleccionado= e.target.getAttribute('data-id');
         const productoEncontrado= catalogoAventura.find((prod)=> prod.id==idSeleccionado);
         carrito.push(productoEncontrado);
-
+        actualizarPantallaCarrito();
     }
 });
-    
+function actualizarPantallaCarrito(){
+    listacarrito.innerHTML='';
+    carrito.forEach((prod)=>{
+        listacarrito.innerHTML+=`
+        <p class="carrito__item">
+            ${prod.nombre} - $${prod.precio}
+        </p>
+        
+        `;
+    });
+    let sumaTotal=0;
+    carrito.forEach((prod)=> sumaTotal += prod.precio);
+    totalcarrito.innerHTML+= sumaTotal; 
+    console.log("contenido del carrito",carrito);
+    console.log("la suma total es: ",sumaTotal);
+}
+
+
+
