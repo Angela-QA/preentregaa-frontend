66c883f9d62eddb12cfa76c8ef19611d8254b45f
Angela <aimendz@hotmail.com>
2026-07-09 08:56:22 -0300

    Diseño horizontal pagina carrito con ministuras de 70px

--- js/tienda.js
+++ js/tienda.js
@@ -5,6 +5,11 @@ const listacarrito= document.querySelector('.carrito__lista');
 const totalcarrito= document.querySelector('#carrito_total-valor');
 const contadorcarrito= document.querySelector('.tienda__carrito-contador');
 
+
+const botoncarrito= document.querySelector('.tienda__carrito-btn');
+const seccioncarrito= document.querySelector('.carrito-seccion');
+
+
 catalogoAventura.forEach((producto)=>{
     contenedorTarjetas.innerHTML +=`
     <div class= "producto">
@@ -26,6 +31,53 @@ contenedorTarjetas.addEventListener("click",(e)=>{
 function actualizarPantallaCarrito(){
     listacarrito.innerHTML='';
     carrito.forEach((prod)=>{
+        listacarrito.innerHTML+=` 
+        <div class= "carrito__item">
+            <img src= "${prod.imagen}" alt= "${prod.nombre}" class= "carrito__item-miniatura" width= 70>
+            <span class= "carrito__item-nombre">
+                ${prod.nombre}
+            </span>
+            <span class= "carrito__item-precio">
+                $${prod.precio}
+            </span>
+            <div class= "carrito__item-cantidades">
+                <button class= "carrito__btn-menos" data-id= "${prod.id}">
+                    -
+                </button>
+                <span class= "carrito__item-cantidad">
+                    1
+                </span>
+                <button class= "carrito__btn-mas" data-id= "${prod.id}">
+                    +
+                </button>
+            </div>
+            
+        </div>
+        
+        `;
+    });
+    let sumaTotal=0;
+    carrito.forEach((prod)=> sumaTotal += prod.precio);
+    totalcarrito.innerHTML= sumaTotal; 
+    contadorcarrito.innerText= carrito.length;
+    if(carrito.length>0){
+        contadorcarrito.style.display='block';
+    } 
+        else{
+            contadorcarrito.style.display= 'none';
+        }
+}
+botoncarrito.addEventListener('click', () =>{
+    seccioncarrito.style.display= 'block';
+});
+
+
+
+        
+
+/*function actualizarPantallaCarrito(){
+    listacarrito.innerHTML='';
+    carrito.forEach((prod)=>{
         listacarrito.innerHTML+=`
         <p class="carrito__item">
             ${prod.nombre} - $${prod.precio}
@@ -35,13 +87,6 @@ function actualizarPantallaCarrito(){
     });
     let sumaTotal=0;
     carrito.forEach((prod)=> sumaTotal += prod.precio);
-    contadorcarrito.innerText= carrito.length;
-    if(carrito.length>0){
-        contadorcarrito.style.display='block';
-    }
     totalcarrito.innerHTML= sumaTotal; 
-    
-}
-
-
+}*/
 
